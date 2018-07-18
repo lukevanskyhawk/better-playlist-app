27ffcb458ca4a3b5534357cf67e6bfe83b7f6403
Lucas Van Skyhawk <luke@vanskyhawk.com>
2018-07-18 09:52:20 -0600

    created playlist, filter, and aggregate components and styled text white and background grey

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './App.css';
+
+let defaultStyle = {
+  color: '#fff'
+};
+
+class Aggregate extends Component {
+  render() {
+    return (
+      <div style={{ ...defaultStyle, width: "40%", display: 'inline-block' }}>
+        <h2>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div style={defaultStyle}>
+        <img />
+        <input type="text" />
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div style={{ ...defaultStyle, display: 'inline-block', width: "25%" }}>
+        <img />
+        <h3>Playlist Name</h3>
+        <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 style={{ ...defaultStyle, 'font-size': '54px' }}>Title</h1>
+        <Aggregate />
+        <Aggregate />
+        <Filter />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+      </div>
+    );
+  }
+}
+
+export default App;
